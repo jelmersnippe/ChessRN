de09c6b80f2001bcc13a9205bf0550c44c99bd1e
jelmersnippe <jelmersnippe@hotmail.com>
2021-03-05 20:53:49 +0100

    Clean up project and dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+    root: true,
+    extends: '@react-native-community',
+    rules: {
+        'prettier/prettier': 0,
+        'react-hooks/exhaustive-deps': 0,
+        'indent': ['error', 4, {'SwitchCase': 1}],
+        'comma-dangle': ['error', 'never'],
+        'quotes': ['error', 'single'],
+        'semi': ['error', 'always'],
+        'jsx-quotes': ['error', 'prefer-single']
+    },
 };
